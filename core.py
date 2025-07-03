745ed4487450cc53990bd7a81c9f32e1b33f79ff
Heitor <161908829+Heitor-M33n@users.noreply.github.com>
2025-07-03 20:43:42 -0300

    Add files via upload

---
+++ core.py
@@ -0,0 +1,153 @@
+from datetime import date
+from datetime import timedelta
+
+from rich.table import Table
+
+from models import Emprestimo
+from models import Obra
+from models import Usuario
+
+class Acervo:
+    """Acervo, realiza e manipula empréstimos, armazena estoque de obras
+
+    Armazena as obras em um dicionário em que as keys são seus ids, realiza a manipulação total dos empréstimos
+    E possui uma classe interna responsável por criar relatórios
+    
+    Attributes:
+        estoque (dict): dicionário no formato {obra.id: obra.quantidade}
+    """
+
+    __slots__ = ['estoque']
+
+    def _relatorio_builder(self):
+        pass
+
+    class _RelatorioBuilder:
+        pass
+
+    def __init__(self) -> None:
+        """Inicializa Acervo
+        """
+        self.estoque = {'Obra': 'qtd_disponível'}
+
+    def __iadd__(self, obra: Obra):
+        """Função do operador +=, adiciona uma obra no estoque, caso já esteja nele, +1 quantidade
+        
+        Args:
+            obra (Obra): obra que está sendo incrementada ao estoque
+            """
+        if obra in self.estoque:
+            self.estoque[obra.titulo] += 1
+            obra.quantidade += 1
+        else:
+            self.estoque[obra.titulo] = obra.quantidade
+
+        return self
+
+    def __isub__(self, obra: Obra):
+        """Função do operador -=, -1 quantidade de uma obra, caso se torne 0, remove completamente do estoque
+        
+        Args:
+            obra (Obra): Obra que está sendo decrementada do estoque
+            """
+        if obra in self.estoque:
+            self.estoque[obra.titulo] -= 1
+            obra.quantidade -= 1
+        else:
+            pass
+
+        if self.estoque[obra.titulo] <= 0:
+            self.estoque.pop(obra.titulo, None)
+
+        return self
+
+    def adicionar(self, obra: Obra) -> None:
+        """Interface explícita para +=
+        
+        Args:
+            obra (Obra): Obra que está sendo incrementada ao estoque
+        """
+        self += obra
+
+    def remover(self, obra: Obra) -> None:
+        """Interface explícita para -=
+        
+        Args:
+            obra (Obra): Obra que está sendo decrementada do estoque
+        """
+        self -= obra
+
+    def emprestar(self, obra: Obra, usuario: Usuario, dias: int = 7) -> None:
+        """Cria um empréstimo com uma data de devolução prevista em 7 dias
+        
+        Como uma obra foi emprestada, decrementa 1 do estoque
+
+        Args:
+            obra (Obra): Obra que será emprestada
+            usuario (Usuario): Usuario realizando o empréstimo
+            dias (int): Dias até a devolução, por padrão, 7
+
+        Raises:
+            ValueError: Ocorre caso a obra não esteja em estoque    
+        """
+        if obra.disponivel(self.estoque):
+            emp = Emprestimo(obra, usuario, date.today() + timedelta(days=dias))
+            self.remover(obra)
+        else:
+            raise ValueError
+
+    def devolver(self, emprestimo: Emprestimo, data_dev: date = date.today()) -> None:
+        """Finaliza um empréstimo, retornando a obra ao estoque
+        
+        Args:
+            emprestimo (Emprestimo): Empréstimo realizado
+            data_dev (date): Data em que a obra foi devolvida ( por padrão o dia atual )
+        """
+        if emprestimo.dias_atraso():
+            self.multar()
+
+        self.adicionar(emprestimo.obra)
+        emprestimo.marcar_devolucao()
+    
+    def renovar(self, emprestimo: Emprestimo, dias_extras: timedelta = timedelta(days=7)) -> None:
+        """Adia a data de devolução de um empréstimo
+        
+        Args:
+            emprestimo (Emprestimo): Empréstimo realizado
+            dias_extras (timedelta): Dias de atraso
+        """
+        if emprestimo.dias_atraso() > dias_extras:
+            return
+            #Atraso tão grande que mesmo renovando ainda está atrasado, ainda estou vendo o que vou fazer
+
+        if emprestimo.dias_atraso():
+            self.multar()
+
+        emprestimo.data_prev_devol += dias_extras
+
+    def valor_multa(self, emprestimo: Emprestimo, data_ref: date = date.today()) -> float:
+        """Calcula a multa sobre o atraso entre a data_prev_dev e a data_ref
+        
+        Args:
+            emprestimo (Emprestimo): Empréstimo realizado
+            data_ref (date): Data de referência ( por padrão o dia atual )
+
+        Returns:
+            float: Retorna o valor da multa em reais
+        """
+        return float(emprestimo.dias_atraso())
+
+    def multar(self, emprestimo: Emprestimo):
+        pass
+
+    def _valida_obra(self, obra: Obra) -> bool:
+        """Valida se uma obra é de fato da classe Obra
+        
+        Args:
+            obra (Obra): Obra que está sendo verificada
+            
+        Raises:
+            TypeError: Caso a obra não seja da classe Obra
+            """
+        if not isinstance(obra, Obra):
+            raise TypeError
---
+++ models.py
@@ -0,0 +1,208 @@
+from datetime import date
+from datetime import timedelta
+import uuid
+
+class BaseEntity:
+    """Classe pai, usada para a criação de um id único e comparação ==.
+
+    Attributes:
+        id (UUID): id gerado pelo uuid4(), único
+        data_criacao (date): data em que a instância foi criada
+    """
+
+    __slots__ = ['id', 'data_criacao']
+
+    def __init__(self) -> None:
+        """Inicializa BaseEntity.
+        """
+        self.id = self._gerar_id
+        self.data_criacao = date.today()
+
+    def __eq__(self, other) -> bool:
+        """Compara instâncias de BaseEntity, verificando se são do mesmo tipo e possuem o mesmo id.
+        
+        Args:
+            other (BaseEntity): a outra instância da comparação
+
+        Returns:
+            bool: True caso iguais ( mesmo id e classe ), False caso contrário
+        """
+        if isinstance(other, type(self)) and other.id == self.id:
+            return True
+        else:
+            return False
+        
+    def _gerar_id(self) -> uuid.UUID:
+        """Gera um id com a biblioteca uuid4(), para usar no __init__.
+        
+        Returns:
+            UUID: um id único gerado pelo uuid4()
+        """
+        return uuid.uuid4()
+
+class Obra(BaseEntity):
+    """Uma obra única usada no acervo.
+    
+    Attributes:
+        id (UUID): id gerado pelo uuid4(), único
+        data_criacao (date): data em que a instância foi criada
+        titulo (str): título da obra
+        autor (str): autor da obra
+        ano (int): ano em que a obra foi publicada
+        categoria (str): categoria da obra
+        quantidade (int): quantidade da obra
+    """
+
+    __slots__ = ['titulo', 'autor', 'ano', 'categoria', 'quantidade']
+
+    def __init__(self, titulo: str, autor: str, ano: int, categoria: str, quantidade: int = 1) -> None:
+        """Inicializa Obra.
+
+        Args:
+            titulo (str): título da obra
+            autor (str): autor da obra
+            ano (int): ano em que a obra foi publicada
+            categoria (str): categoria da obra
+            quantidade (int): quantidade da obra, padrão 1 ( é o esperado na criação da obra )
+        """
+        super().__init__()
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        self.categoria = categoria
+        self.quantidade = quantidade
+
+    def disponivel(self, estoque: dict) -> bool:
+        """Verifica se a obra está no estoque do acervo.
+
+        Args:
+            estoque (dict): acervo.estoque, dicionário com todas as obras e suas quantidades
+
+        Returns:
+            bool: True caso esteja no estoque, False caso contrário
+        """
+        if self.titulo in estoque:
+            return True
+        else: 
+            return False
+        
+    def __str__(self) -> str:
+        """Retorna uma string para representar o objeto no print().
+        
+        Returns:
+            str: Nome e ano da obra
+        """
+        return f'"{self.titulo}", {self.ano}'
+        
+class Usuario(BaseEntity):
+    """Um usuário do serviço de acervo.
+
+    Attributes:
+        id (UUID): id gerado pelo uuid4(), único
+        data_criacao (date): data em que a instância foi criada
+        nome (str): nome do usuário
+        email (str): email do usuário
+    """
+
+    __slots__ = ['nome', 'email']
+
+    def __init__(self, nome: str, email: str) -> None:
+        """Inicializa Usuario.
+        
+        Args:
+            nome (str): nome do usuário
+        email (str): email do usuário
+        """
+
+        super().__init__()
+        self.nome = nome
+        self.email = email
+
+    def __lt__(self, other) -> bool:
+        """Compara instâncias do Usuario, usando seus nomes como parâmetro de verificação.
+        
+        Args:
+            other (Usuario): a outra instância da comparação
+
+        Returns:
+            bool: True caso menor que, False caso contrário
+        """
+        return self.nome < other.nome
+    
+    def __gt__(self, other) -> bool:
+        """Compara instâncias do Usuario, usando seus nomes como parâmetro de verificação.
+        
+        Args:
+            other (Usuario): a outra instância da comparação
+
+        Returns:
+            bool: True caso maior que, False caso contrário
+        """
+        return self.nome > other.nome
+    
+    def __str__(self) -> str:
+        """Retorna uma string para representar o objeto no print().
+        
+        Returns:
+            str: nome do usuário
+        """
+        return self.nome
+    
+class Emprestimo(BaseEntity):
+    """Representa um empréstimo, associando o usuário com a obra emprestada.
+    
+    Attributes:
+        id (UUID): id gerado pelo uuid4(), único
+        data_criacao (date): data em que a instância foi criada
+        obra (Obra): Obra associada ao empréstimo
+        usuario (Usuario): Usuário associada ao empréstimo
+        data_prev_devol (date): Data prevista para devolução
+        data_retirada (date): Data em que ocorreu o empréstimo
+        data_dev_real (date):  Data em que ocorrerá a devolução, maior prioridade, mas por padrão, None
+    """
+
+    __slots__ = ['obra', 'usuario', 'data_prev_devol', 'data_retirada', 'data_dev_real']
+
+    def __init__(self, obra: Obra, usuario: Usuario, data_prev_devol: date = date.today() + timedelta(days=7), data_retirada: date = date.today()) -> None:
+        """Inicializa Emprestimo.
+
+        Args:
+            obra (Obra): Obra associada ao empréstimo
+            usuario (Usuario): Usuário associada ao empréstimo
+            data_prev_devol (date): Data prevista para devolução ( por padrão, 7 dias no futuro )
+            data_retirada (date): Data em que ocorreu o empréstimo ( por padrão o dia atual, date.today() )
+        """
+
+        super().__init__()
+        self.obra = obra
+        self.usuario = usuario
+        self.data_retirada = data_retirada
+        self.data_prev_devol = data_prev_devol
+        self.data_dev_real = None
+
+    def marcar_devolucao(self, data_dev_real: date) -> None:
+        """Registra data de devolução.
+
+        Args:
+            data_dev_real (date): Data de devolução real
+        """
+        self.data_dev_real = data_dev_real
+
+    def dias_atraso(self, data_ref: date = date.today()) -> int:
+        """Calcula quantos dias de atraso entre a data de referência e a data de devolução, retorna 0 caso em dia
+
+        Args:
+            data_ref (date): Data de referência para a comparação, ( por padrão o dia atual )
+
+        Returns:
+            int: Quantidade de dias que a devolução está em atraso, retorna 0 caso esteja em dia
+        """
+        return abs((self.data_prev_devol - data_ref).days)
+
+    def __str__(self) -> str:
+        """Retorna uma string para representar o objeto no print.
+        
+        Returns:
+            str: 'prev: dd/mm'
+        """
+        return f'prev: {date.strftime('%d/%m')}'
